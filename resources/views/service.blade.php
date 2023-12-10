1a7bb6f7f58a29f927f6223619fb216a4343c28b
yazid akbar <mryazidkhan@gmail.com>
2023-12-10 13:01:23 +0700

    first commit

---
+++ resources/views/service.blade.php
@@ -0,0 +1,230 @@
+@extends('layout')
+
+@section('title', 'Bayu Celluler | Service')
+
+@section('content')
+<main>
+  <section class="serviceus mb-9rem">
+    <div class="serviceus-image">
+      <div class="image">
+        <img src="{{ url('images/heroserviceimg.png') }}" alt="hero">
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-5">
+          <div class="serviceus-title">
+            <h2 class="font-jakarta">Layanan Kami</h2>
+            <p>Menerima segala permintaan service atau konsultasi hanya dari rumah kamu. Terpercaya dan cepat!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Part -->
+  <section class="part">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-4">
+          <a href="{{ url('/servicedetail') }}" class="text-decoration-none">
+            <div class="part-card">
+              <div class="image">
+                <img src="{{ url('images/service2.png') }}" alt="part">
+              </div>
+              <div class="body">
+                <h5 class="title font-jakarta">Service Mesin Handphone</h5>
+                <p class="text">Kamera, kelistrikan, dan lainnya</p>
+              </div>
+              <div class="logo">
+                <div class="brand">
+                  <img src="{{ url('images/xiaomi.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/samsung.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/oppo.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/vivo.png') }}" alt="brand">
+                </div>
+              </div>
+            </div>
+          </a>
+        
+        </div>
+        <div class="col-xl-4">
+          <a href="{{ url('/servicedetail') }}" class="text-decoration-none">
+            <div class="part-card">
+              <div class="image">
+                <img src="{{ url('images/service1.png') }}" alt="part">
+              </div>
+              <div class="body">
+                <h5 class="title font-jakarta">Service Layar LCD</h5>
+                <p class="text">Layar LCD, layar dalam, dan lainnya</p>
+              </div>
+              <div class="logo">
+                <div class="brand">
+                  <img src="{{ url('images/xiaomi.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/samsung.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/oppo.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/vivo.png') }}" alt="brand">
+                </div>
+              </div>
+            </div>
+          </a>
+         
+        </div>
+        <div class="col-xl-4">
+          <a href="{{ url('/servicedetail') }}" class="text-decoration-none">
+            <div class="part-card">
+              <div class="image">
+                <img src="{{ url('images/service3.png') }}" alt="part">
+              </div>
+              <div class="body">
+                <h5 class="title font-jakarta">Service Baterai</h5>
+                <p class="text">Ganti, perbaiki baterai dan lainnya</p>
+              </div>
+              <div class="logo">
+                <div class="brand">
+                  <img src="{{ url('images/xiaomi.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/samsung.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/oppo.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/vivo.png') }}" alt="brand">
+                </div>
+              </div>
+            </div>
+          </a>
+       
+        </div>
+        <div class="col-xl-4">
+          <a href="{{ url('/servicedetail') }}" class="text-decoration-none">
+            <div class="part-card">
+              <div class="image">
+                <img src="{{ url('images/service3.png') }}" alt="part">
+              </div>
+              <div class="body">
+                <h5 class="title font-jakarta">Service Baterai</h5>
+                <p class="text">Ganti, perbaiki baterai dan lainnya</p>
+              </div>
+              <div class="logo">
+                <div class="brand">
+                  <img src="{{ url('images/xiaomi.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/samsung.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/oppo.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/vivo.png') }}" alt="brand">
+                </div>
+              </div>
+            </div>
+          </a>
+         
+        </div>
+        <div class="col-xl-4">
+          <a href="{{ url('/servicedetail') }}" class="text-decoration-none">
+            <div class="part-card">
+              <div class="image">
+                <img src="{{ url('images/service3.png') }}" alt="part">
+              </div>
+              <div class="body">
+                <h5 class="title font-jakarta">Service Baterai</h5>
+                <p class="text">Ganti, perbaiki baterai dan lainnya</p>
+              </div>
+              <div class="logo">
+                <div class="brand">
+                  <img src="{{ url('images/xiaomi.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/samsung.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/oppo.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/vivo.png') }}" alt="brand">
+                </div>
+              </div>
+            </div>
+          </a>
+         
+        </div>
+        <div class="col-xl-4">
+          <a href="{{ url('/servicedetail') }}" class="text-decoration-none">
+            <div class="part-card">
+              <div class="image">
+                <img src="{{ url('images/service3.png') }}" alt="part">
+              </div>
+              <div class="body">
+                <h5 class="title font-jakarta">Service Baterai</h5>
+                <p class="text">Ganti, perbaiki baterai dan lainnya</p>
+              </div>
+              <div class="logo">
+                <div class="brand">
+                  <img src="{{ url('images/xiaomi.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/samsung.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/oppo.png') }}" alt="brand">
+                </div>
+                <div class="brand">
+                  <img src="{{ url('images/vivo.png') }}" alt="brand">
+                </div>
+              </div>
+            </div>
+          </a>
+        
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End : Part -->
+  <!-- Reservasi -->
+  <div class="reservasi bg-blacker">
+    <div class="container">
+      <div class="reservasi-wrapper bg-purple">
+        <div class="row">
+          <div class="col-xl-5">
+            <div class="reservasi-cta">
+              <div class="title">
+                <h5 class="font-jakarta">Konsultasikan & Reservasi</h5>
+                <p class="font-jakarta">Hubungi admin kami untuk melakukan konsultasi sebelum melakukan reservasi
+                  layanan kami!</p>
+              </div>
+              <div class="button">
+                <button><a href="{{ url('/#consultation') }}" class="text-decoration-none text-dark">Hubungi Kami</a></button>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-7">
+            <div class="reservasi-image">
+              <img src="{{ url('images/mmockup.png') }}" alt="mmockup">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End : reservasi -->
+</main>
+@endsection
+
+
